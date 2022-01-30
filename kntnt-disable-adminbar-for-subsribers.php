6847327b8b90ecd296c7267c905e8c0db3ebee0c
Thomas Barregren <thomas@kntnt.com>
2022-01-30 14:01:44 +0100

    Create kntnt-disable-adminbar-for-subsribers.php

---
+++ kntnt-disable-adminbar-for-subsribers.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Kntnt Disable Adminbar for Subsribers
+ * Plugin URI:        https://www.kntnt.com/
+ * Description:       Disables adminbar on the front for subscribers.
+ * Version:           1.0.0
+ * Author:            Thomas Barregren
+ * Author URI:        https://www.kntnt.com/
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+defined( 'ABSPATH' ) || die;
+
+add_action( 'wp', function () {
+
+	$user = wp_get_current_user();
+	if ( count( $user->roles ) == 1 && 'subscriber' == $user->roles ) {
+	//	add_filter( 'show_admin_bar', '__return_false' );
+	}
+
+} );
